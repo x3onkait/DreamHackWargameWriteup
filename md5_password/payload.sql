9a603db3d9f5e6f17930e57ad672a764a0661b42
luminosity <47494005+x3onkait@users.noreply.github.com>
2022-03-11 11:47:52 +0900

    Create payload.sql

---
+++ md5_password/payload.sql
@@ -0,0 +1,4 @@
+select * from admin_password where password='md5($ps,true)'
+select * from admin_password where password='�T0D��o#��'or'8'
+select * from admin_password where (False) or (True)
+select * from admin_password where (True)
