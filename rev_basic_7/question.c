6617ea86606d0438bb050a8858f865f7adf71dac
luminosity <47494005+x3onkait@users.noreply.github.com>
2022-03-11 12:00:27 +0900

    Create question.c

---
+++ rev_basic_7/question.c
@@ -0,0 +1,11 @@
+__int64 __fastcall sub_140001000(__int64 a1)
+{
+  int i; // [rsp+0h] [rbp-18h]
+
+  for ( i = 0; (unsigned __int64)i < 0x1F; ++i )
+  {
+    if ( (i ^ (unsigned __int8)__ROL1__(*(_BYTE *)(a1 + i), i & 7)) != byte_140003000[i] )
+      return 0i64;
+  }
+  return 1i64;
+}
